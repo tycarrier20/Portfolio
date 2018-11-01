f291e83b12754f20212ba4ab90c8cfa95c6ca116
Tyler Carrier <32909051+tycarrier20@users.noreply.github.com>
2018-11-01 12:05:39 -0400

    Add files via upload

---
+++ Mobile Applications/Dicee-master/app/src/main/java/com/carrier/dicer/MainActivity.java
@@ -0,0 +1,53 @@
+package com.carrier.dicer;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Log.d("Dicer", "Tha Button Been Pressed Yo");
+
+                Random randomNumberGen = new Random();
+                int randNumber = randomNumberGen.nextInt(6);
+
+                //Log.d("Dicer", "Yo rando numero is: "+randNumber);
+
+                leftDice.setImageResource(diceArray[randNumber]);
+
+                randNumber = randomNumberGen.nextInt(6);
+
+                rightDice.setImageResource(diceArray[randNumber]);
+            }
+        });
+    }
+}
